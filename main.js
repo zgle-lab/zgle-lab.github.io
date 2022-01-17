f97bfa91f4a958095a4204b1e7a96d612b469f28
zglecom <92059787+zglecom@users.noreply.github.com>
2022-01-16 22:25:46 -0700

    uploaded basic html css js files

---
+++ main.js
@@ -0,0 +1,7 @@
+function gimme_time() {
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+    return dateTime;   // The function returns the product of p1 and p2
+  }
